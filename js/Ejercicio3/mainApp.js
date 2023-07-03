ce175ec50366e235ac704cda82f89e758885e9d3
RobertoCarrera <robertocarrera93@gmail.com>
2023-07-03 21:48:02 +0200

    Acabado, falta comentar

--- js/Ejercicio3/mainApp.js
+++ js/Ejercicio3/mainApp.js
@@ -1,9 +1,11 @@
 "use strict";
-let electrodomesticos = [];
+let electrodomesticos = new Array(10);
 let consumos = ["A", "B", "C", "D", "E", "F"];
 let colores = ["Negro", "Rojo", "Azul", "Gris", "Blanco"];
 let randomElectrodomestico = 0;
-for (let i = 0; i < 10; i++) {
+let precioLavadoras = 0;
+let precioTelevisiones = 0;
+for (let i = 0; i < electrodomesticos.length; i++) {
     let consumo = consumos[Math.floor(Math.random() * consumos.length - 1)];
     let color = colores[Math.floor(Math.random() * colores.length - 1)];
     randomElectrodomestico = Math.floor(Math.random() * 3);
@@ -15,11 +17,16 @@ for (let i = 0; i < 10; i++) {
         if (randomElectrodomestico == 1) {
             electrodomesticos[i] = new Lavadora(consumo, color);
             electrodomesticos[i].precioFinal();
+            precioLavadoras += electrodomesticos[i].getPrecioBase();
         }
         else {
             electrodomesticos[i] = new Television(consumo, color);
             electrodomesticos[i].precioFinal();
+            precioTelevisiones += electrodomesticos[i].getPrecioBase();
         }
     }
 }
 console.log(electrodomesticos);
+console.log("El precio total de todos los electrodomésticos son " + (precioLavadoras + precioTelevisiones) + "€");
+console.log("El precio total de las lavadoras son " + precioLavadoras + "€");
+console.log("El precio total de las televisiones son " + precioTelevisiones + "€");
--- src/Ejercicio3/mainApp.ts
+++ src/Ejercicio3/mainApp.ts
@@ -1,10 +1,12 @@
 
-let electrodomesticos: Electrodomestico[] = [];
+let electrodomesticos: Electrodomestico[] = new Array(10);
 let consumos: String[] = ["A", "B", "C", "D", "E", "F"];
 let colores: String[] = ["Negro", "Rojo", "Azul", "Gris", "Blanco"];
 let randomElectrodomestico: number = 0;
+let precioLavadoras: number = 0;
+let precioTelevisiones: number = 0;
 
-for (let i = 0; i < 10; i++) {
+for (let i = 0; i < electrodomesticos.length; i++) {
 
     let consumo: String = consumos[Math.floor(Math.random()*consumos.length-1)];
     let color: String = colores[Math.floor(Math.random()*colores.length-1)];
@@ -21,13 +23,16 @@ for (let i = 0; i < 10; i++) {
 
             electrodomesticos[i] = new Lavadora(consumo, color);
             electrodomesticos[i].precioFinal();
+            precioLavadoras += electrodomesticos[i].getPrecioBase();
         }else{
 
             electrodomesticos[i] = new Television(consumo, color);
             electrodomesticos[i].precioFinal();
+            precioTelevisiones += electrodomesticos[i].getPrecioBase();
         }
     }
-
-
 }
 console.log(electrodomesticos);
+console.log("El precio total de todos los electrodomésticos son "+(precioLavadoras+precioTelevisiones)+"€");
+console.log("El precio total de las lavadoras son "+precioLavadoras+"€");
+console.log("El precio total de las televisiones son "+precioTelevisiones+"€");
